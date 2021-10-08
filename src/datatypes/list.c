67c36fceb874ad33447501a0f32c05a82c2c5a04
Mert <dv19tkn@cs.umu.se>
2021-10-08 14:34:30 +0200

    Added datatype list with a test

---
+++ src/datatypes/list.c
@@ -0,0 +1,184 @@
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+
+/**
+ * @defgroup list_static Static_List
+ *
+ * @brief "list.c" contains the functions used to make a double ended list.
+ * The following lists are using dynamic memory, use it with caustion.
+ *
+ * @author Mert Karkan dv19TKN
+ * @{
+ */
+
+/**
+  *
+  * @brief Clones the given string.
+  *
+  * Gets the value at the given position in the list and returns a copy of it.
+  *
+  * @param Char* The string to be copied.
+  * @return Char* A copy of the given string.
+  */
+static char *clone_string(const char *in)
+{
+    size_t len = strlen(in);
+    char *out = calloc(len + 1, sizeof(char));
+    strcpy(out, in);
+    return out;
+}
+
+/**
+ * @brief Makes a node.
+ *
+ * Creates a node and puts in the value given. The pointers "next" and "prev"
+ * points to NULL on creation.
+ *
+ * <b>OBS</b>: The user has to free up memory either with a function or
+ * manually when destroying the node.
+ *
+ * @param Char* The value of the node.
+ * @return Char* A node where the pointers "next" and "prev" points to NULL.
+ */
+static struct node *make_node(const char *ssn, const char *email, const char *name)
+{
+    struct node *new_node = malloc(sizeof(struct node));
+    new_node->next = NULL;
+    new_node->prev = NULL;
+    new_node->ssn = clone_string(ssn);
+    new_node->email = clone_string(email);
+    new_node->name = clone_string(name);
+
+    return new_node;
+}
+
+/**
+ * @}
+ */
+
+//(The user has to free up memory.)
+List *list_create(void)
+{
+    List *lst = malloc(sizeof(List));
+    lst->head = (struct node){
+        .next = &lst->head,
+        .prev = &lst->head,
+        .ssn = NULL,
+        .email = NULL,
+        .name = NULL};
+
+    return lst;
+}
+
+//(FREEING UP MEMORY.)
+void list_destroy(List *lst)
+{
+    ListPos pos = list_first(lst);
+
+    while (!list_is_empty(lst))
+    {
+        pos = list_remove(pos);
+    }
+
+    free(lst);
+}
+
+bool list_is_empty(const List *lst)
+{
+    return lst->head.next == &lst->head;
+}
+
+ListPos list_first(List *lst)
+{
+    ListPos pos = {
+        .node = lst->head.next};
+
+    return pos;
+}
+
+ListPos list_end(List *lst)
+{
+    ListPos pos = {
+        .node = &lst->head};
+
+    return pos;
+}
+
+bool list_pos_equal(ListPos p1, ListPos p2)
+{
+    if (p1.node == p2.node)
+    {
+        return true;
+    }
+    return false;
+}
+
+ListPos list_next(ListPos pos)
+{
+    pos.node = pos.node->next;
+    return pos;
+}
+
+ListPos list_prev(ListPos pos)
+{
+    pos.node = pos.node->prev;
+    return pos;
+}
+
+ListPos list_insert(ListPos pos, const char *ssn, const char *email, const char *name)
+{
+    // Create a new node.
+    struct node *node = make_node(ssn, email, name);
+
+    // Find nodes before and after (may be the same node: the head of the list).
+    struct node *before = pos.node->prev;
+    struct node *after = pos.node;
+
+    // Link to node after.
+    node->next = after;
+    after->prev = node;
+
+    // Link to node before.
+    node->prev = before;
+    before->next = node;
+
+    // Return the position of the new element.
+    pos.node = node;
+    return pos;
+}
+
+//(FREEING UP MEMORY.)
+ListPos list_remove(ListPos pos)
+{
+    pos.node->prev->next = pos.node->next;
+    pos.node->next->prev = pos.node->prev;
+
+    ListPos new_pos = {pos.node->next};
+
+    //testa och se ifall det funkar utan
+    free(pos.node->ssn);
+    free(pos.node->email);
+    free(pos.node->name);
+    free(pos.node);
+
+    return new_pos;
+}
+
+const char *list_inspect_ssn(ListPos pos)
+{
+    char *ssn = pos.node->ssn;
+    return ssn;
+}
+
+const char *list_inspect_email(ListPos pos)
+{
+    char *email = pos.node->email;
+    return email;
+}
+
+const char *list_inspect_name(ListPos pos)
+{
+    char *name = pos.node->name;
+    return name;
+}
---
+++ src/datatypes/list.h
@@ -0,0 +1,217 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdbool.h>
+
+/**
+ * @defgroup list list.h
+ * @brief The header file for most the functions used in the list.
+ * The list construction is double ended and stores one value for the user
+ * to store information in. It also contains two pointers to move forward and
+ * backwards. Dynamic memory is used in these following functions so the user
+ * has to be caustios with memory usage.
+ *
+ * @author Mert Karkan dv19TKN
+ * @{
+ */
+
+/**
+ * @brief The structure for a "node".
+ *
+ * The structer has a pointer to node called "next", which is pointing
+ * to the next node in the list.
+ *
+ * There is a pointer to node called "prev", which is pointing to
+ * the previous node in the list.
+ *
+ * There is also "value" which is a pointer to char called "value",
+ * that stores the users value.
+ *
+ */
+struct node
+{
+    struct node *next;
+    struct node *prev;
+    char *ssn;
+    char *email;
+    char *name;
+};
+
+/**
+ * @brief The structure for a "list".
+ *
+ * The structure has a "head" of type "node", which refers to the first
+ * element in the list.
+ *
+ * <b>OBS</b>: The head can also point to "prev" which refers to the last element
+ * in the list.
+ */
+typedef struct list
+{
+    struct node head;
+} List;
+
+/**
+ * @brief The struct for a "list_pos".
+ *
+ * The structer has a pointer to "node", which refers to the position in
+ * the list.
+ *
+ */
+typedef struct list_pos
+{
+    struct node *node;
+} ListPos;
+
+/**
+ * @brief Creates a list.
+ *
+ * Creates the head of the list. The head is refered to as the first elements
+ * in the list, which is needed to keep track on the elemnts position in the
+ * list.
+ *
+ * <b>OBS</b>: The user has to free up memory either with a function or manually.
+ * @param Void
+ * @return *List A pointer to the head of the list
+ */
+List *list_create(void);
+
+/**
+ * @brief Deallocate the list.
+ *
+ * Deallocate the list (and all of its values, if any) using the function
+ * "list_remove". This function also uses "list_first" and "list_next" to go
+ * through each element in the list.
+ *
+ * <b>OBS</b>: Freeing up memory, use cautiously.
+ * @param List* Pointer to a list.
+ * @return Void
+ */
+void list_destroy(List *lst);
+
+/**
+ * @brief Checks if the list is empty.
+ *
+ * Checks if the list is empty by looking at next element of the head.
+ * Returns "true" if list is empty, or "false" otherwise.
+ * @param List* A pointer to the head of the list.
+ * @return Bool True if the list is empty.
+ */
+bool list_is_empty(const List *lst);
+
+/**
+ * @brief Gets the position of the first element.
+ *
+ * Gets the position of the first element for a given list, which is the
+ * element next to head.
+ *
+ * @param List* A pointer to the head of the list.
+ * @return ListPos The position of the first elemennt in the list.
+ */
+ListPos list_first(List *lst);
+
+/**
+ * @brief Get the position <b>after</b> the last element.
+ *
+ * Get the position <b>after</b> the last element, which is also the head
+ * of the list.
+ *
+ * @param List A pointer to the head of the list.
+ * @return ListPos The position of the head of the list.
+ */
+ListPos list_end(List *lst);
+
+/**
+ * @brief Check equality between two positions.
+ *
+ * Check equality between two given positions in the list. Returns "true"
+ * as a boolean value if the nodes are equal.
+ *
+ * @param ListPos The first position.
+ * @param ListPos The second position.
+ * @return Bool Returns true if the positions are equal, else false.
+ */
+bool list_pos_equal(ListPos p1, ListPos p2);
+
+/**
+ * @brief Goes to the next position.
+ *
+ * Goes to the next position in the list for a given node.
+ *
+ * @param ListPos The current position.
+ * @return ListPos The position next to the current position.
+ */
+ListPos list_next(ListPos pos);
+
+/**
+ * @brief Goes to the previous position.
+ *
+ * Goes to the previous position in the list for a given node.
+ *
+ * @param ListPos The current position.
+ * @return ListPos The previous position to the current position.
+ */
+ListPos list_prev(ListPos pos);
+
+/**
+ * @brief Inserts the given value and returns the position of the new element.
+ *
+ * Insert the value before the position and return the position of the new
+ * element.
+ *
+ * @param ListPos The position of the element.
+ * @param Char* The value to be added.
+ * @return ListPos The position of the new element.
+ */
+ListPos list_insert(ListPos pos, const char *ssn, const char *email, const char *name);
+
+/**
+ * @brief Removes the node for the given position.
+ *
+ * Removes the value at the position and return the position of the next
+ * element in the list.
+ *
+ * <b>OBS</b>: Freeing up memory, use cautiously.
+ *
+ * @param ListPos The position of the element.
+ * @return ListPos The position next to the removed element.
+ */
+ListPos list_remove(ListPos pos);
+
+/**
+ * @brief Gets the social security number at that position.
+ *
+ * Gets the social security number of the person at that 
+ * position of the list.
+ *
+ * @param ListPos The position of the element.
+ * @return Char The SSN of the person.
+ */
+const char *list_inspect_ssn(ListPos pos);
+
+/**
+ * @brief Gets the E-Mail at that position.
+ *
+ * Gets the E-Mail of the person at that 
+ * position of the list.
+ *
+ * @param ListPos The position of the element.
+ * @return Char The email of the person.
+ */
+const char *list_inspect_email(ListPos pos);
+
+/**
+ * @brief Gets the name at the position.
+ *
+ * Gets the name at the given position in the list.
+ *
+ * @param ListPos The position of the element.
+ * @return Char The name at the given position.
+ */
+const char *list_inspect_name(ListPos pos);
+
+/**
+ * @}
+ */
+
+#endif /* LIST_H */
