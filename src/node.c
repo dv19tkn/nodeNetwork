10a937b283ac888f6dfda41b7ff9a546ea52c98a
Mathias <mathias.hedqvist@gmail.com>
2021-10-08 14:59:05 +0200

    node 0.1

--- src/node.c
+++ src/node.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 typedef enum {
     q1,
@@ -53,101 +60,161 @@ eSystemState gotoStateQ1(void) {
 }
 
 eSystemState gotoStateQ2(void) {
-    return q1;
+    return q2;
 }
 
 eSystemState gotoStateQ3(void) {
-    return q1;
+    return q3;
 }
 
 eSystemState gotoStateQ4(void) {
-    return q1;
+    return q4;
 }
 
 eSystemState gotoStateQ5(void) {
-    return q1;
+    return q5;
 }
 
 eSystemState gotoStateQ6(void) {
-    return q1;
+    return q6;
 }
 
 eSystemState gotoStateQ7(void) {
-    return q1;
+    return q7;
 }
 
 eSystemState gotoStateQ8(void) {
-    return q1;
+    return q8;
 }
 
 eSystemState gotoStateQ9(void) {
-    return q1;
+    return q9;
 }
 
 eSystemState gotoStateQ10(void) {
-    return q1;
+    return q10;
 }
 
 eSystemState gotoStateQ11(void) {
-    return q1;
+    return q11;
 }
 
 eSystemState gotoStateQ12(void) {
-    return q1;
+    return q12;
 }
 
 eSystemState gotoStateQ13(void) {
-    return q1;
+    return q13;
 }
 
 eSystemState gotoStateQ14(void) {
-    return q1;
+    return q14;
 }
 
 eSystemState gotoStateQ15(void) {
-    return q1;
+    return q15;
 }
 
 eSystemState gotoStateQ16(void) {
-    return q1;
+    return q16;
 }
 
 eSystemState gotoStateQ17(void) {
-    return q1;
+    return q17;
 }
 
 eSystemState gotoStateQ18(void) {
-    return q1;
+    return q18;
+}
+
+eSystemState exitState(void) {
+    return lastState;
 }
 
 
 static afEventHandler stateMachine = {
     [q1] = { [eventDone] = gotoStateQ2 }, //Q2
     [q2] = { [eventStunResponse] = gotoStateQ3}, //Q3 
-    [q3] = { [eventNodeResponse] = }, 
-    [q3] = { [eventNodeResponseEmpty] = }, 
-    [q4] = { [eventDone] = }, //Q6 
-    [q6] = { [eventInsertLookupRemove] = }, //Q9 
-    [q6] = { [eventShutDown] = }, //Q10
-    [q6] = { [eventJoin] = }, //Q12 
-    [q6] = { [eventNewRange] = }, //Q15
-    [q6] = { [eventLeaving] = }, //Q16
-    [q6] = { [eventCloseConnection] = }, //Q17 
-    [q9] = { [eventDone] = }, //Q6 
-    [q10] = { [eventConnected] = }, //Q11
-    [q10] = { [eventNotConnected] = },  //exit
-    [q11] = { [eventNewRangeResponse] = }, //Q18 
-    [q12] = { [eventNotConnected] = }, //Q5
-    [q12] = { [eventMaxNode] = }, //Q13
-    [q12] = { [eventNotMaxNode] = }, //Q14
-    [q18] = { [eventDone] = }, //exit
-    [q5] = { [eventDone] = }, //Q6
-    [q13] = { [eventDone] = }, //Q6
-    [q14] = { [eventDone] = }, //Q6
+    [q3] = { [eventNodeResponse] = gotoStateQ7}, 
+    [q3] = { [eventNodeResponseEmpty] = gotoStateQ4}, 
+    [q4] = { [eventDone] = gotoStateQ6}, //Q6 
+    [q6] = { [eventInsertLookupRemove] = gotoStateQ9}, //Q9 
+    [q6] = { [eventShutDown] = gotoStateQ10}, //Q10
+    [q6] = { [eventJoin] = gotoStateQ12}, //Q12 
+    [q6] = { [eventNewRange] = gotoStateQ15}, //Q15
+    [q6] = { [eventLeaving] = gotoStateQ16}, //Q16
+    [q6] = { [eventCloseConnection] = gotoStateQ17}, //Q17 
+    [q9] = { [eventDone] = gotoStateQ6}, //Q6 
+    [q10] = { [eventConnected] = gotoStateQ11}, //Q11
+    [q10] = { [eventNotConnected] = exitState},  //exit
+    [q11] = { [eventNewRangeResponse] = gotoStateQ18}, //Q18 
+    [q12] = { [eventNotConnected] = gotoStateQ5}, //Q5
+    [q12] = { [eventMaxNode] = gotoStateQ13}, //Q13
+    [q12] = { [eventNotMaxNode] = gotoStateQ14}, //Q14
+    [q18] = { [eventDone] = exitState}, //exit
+    [q5] = { [eventDone] = gotoStateQ6}, //Q6
+    [q13] = { [eventDone] = gotoStateQ6}, //Q6
+    [q14] = { [eventDone] = gotoStateQ6} //Q6
+};
+
+static void exit_on_error(const char *title) {
+	perror(title);
+	exit(1);
+}
+
+static void exit_on_error_custom(const char *title, const char *detail) {
+	fprintf(stderr, "%s:%s\n", title, detail);
+	exit(1);
 }
 
 int main(int argc, char *argv[]) {
+    char eCheckString[INET_ADDRSTRLEN]; //used to error check ip addresses 
+
+    fprintf(stderr, "starting node\n");
+
+    // eSystemState currentState = q1;
+    // eSystemEvent nextEvent = lastEvent;
+
+    //Handle input
+    if (argc != 3) {
+        exit_on_error_custom("Usage: ", " node <Tracker Address> <Tracker Port>");
+    }
 
 
+    //Read arguments to trackerAddress
+    struct sockaddr_in trackerAddress;
+    socklen_t socketUdpALength = sizeof(trackerAddress);
+    memset(&trackerAddress, 0, socketUdpALength);
+
+    trackerAddress.sin_family = AF_INET;
+
+    //set input address to network byte order
+    if (inet_aton(argv[1], (struct in_addr *) &trackerAddress) == 0) {
+        exit_on_error_custom("inet_aton", argv[1]);
+    }
+    fprintf(stderr, "address to byte order: %d\n", trackerAddress.sin_addr.s_addr);
+
+    //set input port to network byte order
+    trackerAddress.sin_port = htons(strtol(argv[2], NULL, 10));
+    fprintf(stderr, "tracker port created: %d\n", ntohs(trackerAddress.sin_port));
+
+    //get the set address in host byte order and print to verify
+    inet_ntop(AF_INET, &trackerAddress, eCheckString, INET_ADDRSTRLEN);
+    if (eCheckString == NULL) {
+        exit_on_error("inet_ntop");
+    }
+    fprintf(stderr, "tracker address created: %s:%d\n", eCheckString, ntohs(trackerAddress.sin_port));
+
+
+    //Set up sockets
+    //Set up udp socket to tracker
+    int socketUdpTracker = socket(AF_INET, SOCK_DGRAM, 0);
+    if (socketUdpTracker == -1) {
+        exit_on_error("socket udp tracker");
+    }
+    fprintf(stderr, "socket created\n");
+
+
+    fprintf(stderr, "exiting node\n");
     return 0;
 }
