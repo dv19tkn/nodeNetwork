82b9be1b0c143ef2a69749424de6b6d8891ead09
Mathias <mathias.hedqvist@gmail.com>
2021-10-10 21:39:09 +0200

    node 0.2

--- src/node.c
+++ src/node.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// #include <unistd.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/ip.h>
 #include <arpa/inet.h>
+#include "pdu.h"
 
 typedef enum {
     q1,
@@ -168,12 +170,13 @@ static void exit_on_error_custom(const char *title, const char *detail) {
 }
 
 int main(int argc, char *argv[]) {
-    char eCheckString[INET_ADDRSTRLEN]; //used to error check ip addresses 
+    // char eCheckString[INET_ADDRSTRLEN]; //used to error check ip addresses 
+    char *eCheckString;
+    // eSystemState currentState = q1;
+    // eSystemEvent nextEvent = lastEvent;
 
     fprintf(stderr, "starting node\n");
 
-    // eSystemState currentState = q1;
-    // eSystemEvent nextEvent = lastEvent;
 
     //Handle input
     if (argc != 3) {
@@ -183,38 +186,111 @@ int main(int argc, char *argv[]) {
 
     //Read arguments to trackerAddress
     struct sockaddr_in trackerAddress;
-    socklen_t socketUdpALength = sizeof(trackerAddress);
-    memset(&trackerAddress, 0, socketUdpALength);
+    memset(&trackerAddress, 0, sizeof(trackerAddress));
+    socklen_t trackerAddressLen = sizeof(trackerAddress);
 
     trackerAddress.sin_family = AF_INET;
-
-    //set input address to network byte order
-    if (inet_aton(argv[1], (struct in_addr *) &trackerAddress) == 0) {
+    trackerAddress.sin_port = htons(strtol(argv[2], NULL, 10));
+    if (inet_aton(argv[1], &trackerAddress.sin_addr) == 0) {
         exit_on_error_custom("inet_aton", argv[1]);
     }
-    fprintf(stderr, "address to byte order: %d\n", trackerAddress.sin_addr.s_addr);
-
-    //set input port to network byte order
-    trackerAddress.sin_port = htons(strtol(argv[2], NULL, 10));
-    fprintf(stderr, "tracker port created: %d\n", ntohs(trackerAddress.sin_port));
 
     //get the set address in host byte order and print to verify
-    inet_ntop(AF_INET, &trackerAddress, eCheckString, INET_ADDRSTRLEN);
+    eCheckString = inet_ntoa(trackerAddress.sin_addr);
     if (eCheckString == NULL) {
-        exit_on_error("inet_ntop");
+        exit_on_error("inet_ntoa");
     }
     fprintf(stderr, "tracker address created: %s:%d\n", eCheckString, ntohs(trackerAddress.sin_port));
 
 
-    //Set up sockets
     //Set up udp socket to tracker
-    int socketUdpTracker = socket(AF_INET, SOCK_DGRAM, 0);
+    int socketUdpTracker = socket(AF_INET, SOCK_DGRAM, 0); //IPPROTO_UDP??
     if (socketUdpTracker == -1) {
         exit_on_error("socket udp tracker");
     }
     fprintf(stderr, "socket created\n");
 
+    //Send STUN_LOOKUP
+    struct STUN_LOOKUP_PDU lookupMessage = {STUN_LOOKUP};
+    socklen_t lookupSize = sizeof(lookupMessage);
 
+    if (sendto(socketUdpTracker, &lookupMessage, lookupSize, 0, (struct sockaddr *) &trackerAddress, trackerAddressLen) == -1) {
+        exit_on_error("send stun lookup error");
+    }
+    fprintf(stderr, "stun lookup sent: %d\n", lookupMessage.type);
+
+    //Receive STUN_RESPONS
+    struct STUN_RESPONSE_PDU responseMessage;
+    socklen_t responseSize = sizeof(responseMessage);
+
+    if (recvfrom(socketUdpTracker, &responseMessage, responseSize, 0, (struct sockaddr *) &trackerAddress, &responseSize) == -1) {
+        exit_on_error("recv stun response error");
+    }
+    fprintf(stderr, "recieved type: %d\n", responseMessage.type);
+
+    struct sockaddr_in receivedAddress;
+    memset(&receivedAddress, 0, sizeof(receivedAddress));
+    receivedAddress.sin_family = AF_INET;
+    receivedAddress.sin_addr.s_addr = responseMessage.address;
+    eCheckString = inet_ntoa(receivedAddress.sin_addr);
+    if (eCheckString == NULL) {
+        exit_on_error("inet_ntoa");
+    }
+    fprintf(stderr, "received address: %s\n", eCheckString);
+
+
+    //Init self address
+    struct NET_GET_NODE_PDU getNodeMessage = {NET_GET_NODE};
+    socklen_t getNodeSize = sizeof(getNodeMessage);
+
+    if (sendto(socketUdpTracker, &getNodeMessage, getNodeSize, 0, (struct sockaddr *) &trackerAddress, trackerAddressLen) == -1) {
+        exit_on_error("send get node error");
+    }
+    fprintf(stderr, "get node sent\n");
+
+    struct NET_GET_NODE_RESPONSE_PDU getNodeResponse;
+    socklen_t getNodeResponseSize = sizeof(getNodeResponse);
+
+    if (recvfrom(socketUdpTracker, &getNodeResponse, getNodeResponseSize, 0, (struct sockaddr *) &trackerAddressLen, &getNodeResponseSize) == -1) {
+        exit_on_error("recv get node response error");
+    }
+    fprintf(stderr, "received type: %d\n", getNodeResponse.type);
+
+    struct sockaddr_in preNodeAddress;
+    memset(&preNodeAddress, 0, sizeof(preNodeAddress));
+    preNodeAddress.sin_family = AF_INET;
+    preNodeAddress.sin_port = getNodeResponse.port;
+    preNodeAddress.sin_addr.s_addr = getNodeResponse.address;
+
+    if (preNodeAddress.sin_port == 0 && preNodeAddress.sin_addr.s_addr == 0) {
+        fprintf(stderr, "got empty reponse\n");
+    } else {
+        fprintf(stderr, "received address: %s:%d\n", inet_ntoa(preNodeAddress.sin_addr), ntohs(preNodeAddress.sin_port));
+    }
+
+    //Init TCP C (GÖRS SENARE)
+    // int socketTcpC = socket(AF_INET, SOCK_STREAM, 0);
+    // if (socketTcpC == -1) {
+    //     exit_on_error("socket");
+    // }
+    // fprintf(stderr, "socket created\n");
+
+    // struct sockaddr_in socketCAddress;
+    // memset(&socketCAddress, 0, sizeof(socketCAddress));
+    // socketCAddress.sin_family = AF_INET;
+    // socketCAddress.sin_port = 0;
+    // socketCAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // socklen_t socketTcpCLen = sizeof(socketCAddress);
+
+    // if (bind(socketTcpC, (struct sockaddr *) &socketCAddress, socketTcpCLen) == -1) {
+    //     exit_on_error("bind");
+    // }
+    // fprintf(stderr, "bind done\n");
+
+
+    close(socketUdpTracker);
+    // close(socketTcpC);
     fprintf(stderr, "exiting node\n");
     return 0;
 }
